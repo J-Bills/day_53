dca3a0454cbd0850076a4ba42af4a8d401d819b3
J-Bills <billsjamal@gmail.com>
2024-06-05 01:42:33 -0700

    finishing sf zillow rent bot

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from dataclasses import dataclass, fields
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from selectolax.parser import HTMLParser
 
 
@@ -43,30 +44,29 @@ class zillowScraper():
 class googleFormInput():
     def __init__(self) -> None:
         self.chrome_options = webdriver.ChromeOptions()
+        self.chrome_options.add_argument("window-size=1200x600")
         self.chrome_options.add_experimental_option('detach', True)
         #chrome_options.add_argument('--headless')
         self.driver = webdriver.Chrome(options=self.chrome_options)
         self.driver.get('https://forms.gle/RYsCCz1QF3jSkJJPA')
-            
+        self.driver.implicitly_wait(3)
+        
     #Will loop through and input all data returned from scraping
     def inputRentalListingInfo(self, rental_listings) -> None:
         for listing in rental_listings:
-            for field in fields(listing):
+            inputs = self.driver.find_elements(By.XPATH, value="//input[contains(@type, 'text')]")
+            for i,field in enumerate(fields(listing)):
                 field_name = field.name
                 field_value = getattr(listing, field_name)
-                input = self.driver.find_element()
-                ActionChains(self.driver)\
-                .move_to_element(input)\
-                .pause(1)\
-                .send_keys(field_value)\
-                .pause(1)\
-                .send_keys(Keys.TAB)\
-                .pause(1)
+                print(f'{i}  fieldname: {field_name} : {field_value}')
+                WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((inputs[i])))
+                inputs[i].send_keys(field_value)
+                self.driver.implicitly_wait(2)
             submit = self.driver.find_element(By.XPATH, '//span[text()="Submit"]').click()
             self.driver.implicitly_wait(5)
             refresh = self.driver.find_element(By.TAG_NAME, 'a').click()
             self.driver.implicitly_wait(5)
-    print('finished')
+        self.driver.quit()
 
 def main():
     zillow_scraper = zillowScraper()
